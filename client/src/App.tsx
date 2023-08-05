85d47dff9705ab8d489443addba75a362e14c059
Earl Hans Genoso <earlhansg@gmail.com>
2023-08-05 08:42:58 +0800

    Client - The socket.io-client has been configured and is effectively communicating with our backend server.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { io, Socket } from "socket.io-client";
+
+const socket: Socket = io("http://localhost:3001");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  // useEffect(() => {
+  //   // You can use the 'socket' object here for your socket operations
+    
+  //   return () => {
+  //     // Clean up the socket connection if needed
+  //     socket.disconnect();
+  //   };
+  // }, []);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="joinChatContainer">
+        <h3>Join A Chat</h3>
+        <input
+          type="text"
+          placeholder="John..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+        <button onClick={joinRoom}>Join A Room</button>
+      </div>
     </div>
   );
 }
