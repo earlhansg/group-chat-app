f66545934b0ff606efb32e5e35fa6c5cdaed8902
Earl Hans Genoso <earlhansg@gmail.com>
2023-08-05 08:07:11 +0800

    Server - initial setup for socket io

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,42 @@
 import express, {Express, Request, Response} from 'express'
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io'
 
 const port = 3001;
 const app: Express = express();
+app.use(cors());
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"] 
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+  
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+})
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello from Express Plus Typescriptsss and more");
 });
 
-app.listen(port, ()=> {
+server.listen(port, ()=> {
     console.log(`now listening to ${port}`);
 })
